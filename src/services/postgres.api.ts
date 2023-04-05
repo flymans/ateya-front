c924e1ac246cc2bba10b68489cf6e9450b729f71
Aleksandr Panteleev <a.panteleev@atol.ru>
2023-04-05 03:40:18 +0300

    feat: add linting

--- src/services/postgres.api.ts
+++ src/services/postgres.api.ts
@@ -7,13 +7,10 @@ interface ProductData {
 
 export const saveDataToPostgreSQL = async (data: ProductData) => {
   try {
-    console.log(process.env.REACT_APP_API_URL);
-    console.log(process.env.REACT_APP_FRONT_URL);
-    console.log(process.env.NODE_ENV);
     const response = await fetch(`${process.env.REACT_APP_API_URL}products`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     });
@@ -21,6 +18,6 @@ export const saveDataToPostgreSQL = async (data: ProductData) => {
     const id = await response.text();
     return id;
   } catch (error) {
-    console.error("Error saving data to PostgreSQL", error);
+    console.error('Error saving data to PostgreSQL', error);
   }
 };
